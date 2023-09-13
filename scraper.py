f054ac0dd4a9bac4f67c69cf435b667846c1c0c7
Hiba Mughal <hibgar@gmail.com>
2023-09-12 22:17:38 -0700

    cleaned code

--- scraper.py
+++ scraper.py
@@ -10,13 +10,6 @@ driver.get('https://www.goodreads.com/review/list/99850740-hiba?ref=nav_mybooks&
 print(driver.title)
 
 booksTable = driver.find_element(By.ID, "booksBody")
-#for item in booksTable.find_elements(By.CSS_SELECTOR,'tr'):
-    #print(item.text)
-    #if item.get_attribute("class") == "field title":
-        #print(item.text)
-        #value = item.find_element(By.CLASS_NAME, "value")
-        #innerTitleItem = value.find_element(By.CSS_SELECTOR, "a")
-        #print(innerTitleItem)
 
 rows = booksTable.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
 for row in rows:
