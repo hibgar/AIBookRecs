3bd155e5fc1b0b768152c7dc4c8ff4fce627677e
Hiba Mughal <hibgar@gmail.com>
2023-09-12 22:16:43 -0700

    prints all the titles in table

--- scraper.py
+++ scraper.py
@@ -4,14 +4,24 @@ from selenium.webdriver.common.by import By
 import time
 import bs4
 
-PATH = "/home/geckodriver"
 driver = webdriver.Firefox()
 
-driver.get('https://www.goodreads.com/review/list/99850740-hiba?ref=nav_mybooks&shelf=favorites')
+driver.get('https://www.goodreads.com/review/list/99850740-hiba?ref=nav_mybooks&shelf=favorites') #open GR site with the list
 print(driver.title)
 
-table = driver.find_element(By.ID, "books")
+booksTable = driver.find_element(By.ID, "booksBody")
+#for item in booksTable.find_elements(By.CSS_SELECTOR,'tr'):
+    #print(item.text)
+    #if item.get_attribute("class") == "field title":
+        #print(item.text)
+        #value = item.find_element(By.CLASS_NAME, "value")
+        #innerTitleItem = value.find_element(By.CSS_SELECTOR, "a")
+        #print(innerTitleItem)
 
-print(table)
-#time.sleep(3)
-driver.close()
+rows = booksTable.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
+for row in rows:
+    # Get the book names (all the 4th column = index 3)
+    col = row.find_elements(By.TAG_NAME, "td")[3]
+    print(col.text) #prints text from the element
+
+driver.close() # close the browser
