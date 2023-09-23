5e70712c1a1351c111992ee754b9536372f6ccea
Hiba Mughal <hibgar@gmail.com>
2023-09-22 18:58:19 -0700

    form loads on screen and returns books that are recommended

--- flaskr/scraper.py
+++ flaskr/scraper.py
@@ -1,17 +1,27 @@
+from flask import render_template, Blueprint
 from selenium import webdriver
+from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+bp = Blueprint('scraper', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
 
 # Create FirefoxOptions object
 firefox_options = Options()
 firefox_options.add_argument('--headless')  # Run Firefox in headless mode
 # Create a WebDriver instance using the Firefox driver and options
-#driver = webdriver.Firefox(options=firefox_options)
+driver = webdriver.Firefox(options=firefox_options)
 
-driver = webdriver.Firefox()
-#userFavoriteGenres = {"Fiction" : 1, 'Young Adult' : 2, 'Fantasy' : 3}
-userFavoriteGenres = {}
+#driver = webdriver.Firefox()
+userFavoriteGenres = {'Fiction': 7, 'Feminism': 1, 'Contemporary': 5, 'Literary Fiction': 2, 'Asia': 1, 'Adult': 2, 'Audiobook': 6, 'Science Fiction': 3, 'Fantasy': 4, 'Dystopia': 3, 'Romance': 6, 'Young Adult': 6, 'Young Adult Contemporary': 2, 'Realistic Fiction': 2, 'Young Adult Romance': 3, 'Young Adult Fantasy': 1, 'Magic': 1, 'High Fantasy': 1}
+#userFavoriteGenres = {}
 bookScores = {}
 
 def findBookGenres(bookDriver, dict):
@@ -36,7 +46,7 @@ def analyzeUserList():
     for row in rows:
         # Get the book names (all the 4th column = index 3)
         col = row.find_elements(By.TAG_NAME, "td")[3]
-        print(col.text)  # prints title of each book
+        #print(col.text)  # prints title of each book
 
         book_title_element = col.find_element(By.TAG_NAME, "a")
         # Extract the book title text
@@ -47,7 +57,7 @@ def analyzeUserList():
         book_link = book_title_element.get_attribute('href')
 
         # New browser to loop thru the books so that the for loop of the initial page is preserved
-        driver2 = webdriver.Firefox()
+        driver2 = webdriver.Firefox(options=firefox_options)
         # Navigate to a new URL in the newly opened window or tab
         driver2.get(book_link)
         # find all the most common genres and save it into listGenres
@@ -78,6 +88,7 @@ def analyzePublicList():
         # New browser to loop thru the books so that the for loop of the initial page is preserved
         driver2 = webdriver.Firefox()
         # Navigate to a new URL in the newly opened window or tab
+        print(book_link)
         driver2.get(book_link)
         score = 0
 
@@ -88,15 +99,17 @@ def analyzePublicList():
             if nameOfGenre in userFavoriteGenres:
                 score += 1
 
-        if score > 0 and float(book_rating) > 3.00:
+        if score > 0: #and (float(book_rating) > 3.00 if book_rating else True):
             bookScores[book_title] = score
 
         driver2.close()
 
     print("Based on your favorite genres, you would enjoy the following books, starting with the best recommendation: ")
     for rec in bookScores:
-        print(rec)
+        print(rec, "with a score of", bookScores[rec])
+    print(bookScores)
 
-analyzeUserList()
+
+#analyzeUserList()
 analyzePublicList()
 driver.close()  # close the browser
