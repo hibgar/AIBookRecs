345e98e0e834a82c7d4bc13ee253705c3f27baf8
Hiba Mughal <hibgar@gmail.com>
2023-12-25 18:22:16 -0800

    flask web app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,10 @@
 import os
+import sqlite3
+import traceback
 
-from flask import Flask
+from flask import Flask, request, flash, redirect, url_for, render_template
+
+from flaskr.scraper import analyzeUserList
 
 
 def create_app(test_config=None):
@@ -32,6 +36,43 @@ def create_app(test_config=None):
     from . import scraper
     app.register_blueprint(scraper.bp)
     app.add_url_rule('/', endpoint='index')
+
+    from . import db
+    db.init_app(app)
+
+    @app.route('/process_user_list', methods=['POST'])
+    def process_form_user():
+        user_list = request.form['userList']  # Assuming 'userList' is the name of your form field
+        #analyzeUserList(user_list)  # Pass the user input to the function in scraper.py
+        #return f'User list submitted: {user_list}'
+        return user_list
+
+    @app.route('/', methods=['GET'])
+    def create():
+        if request.method == 'POST':
+
+
+            try:
+                link1 = request.form.get('userList')
+                link2 = request.form.get('publicList')
+
+                # Validate the form data
+                if not link1 or not link2:
+                    print('Name and email are required fields.')
+                print(link1, link2)
+                # Add data to the database
+                conn = sqlite3.connect('your_database.db')
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO links (link1, link2) VALUES (?, ?)", (link1, link2))
+                conn.commit()
+                conn.close()
+
+                print('Data submitted successfully!')
+            except Exception as e:
+                print('An error occurred while processing the form.', str(e))
+                traceback.print_exc()  # Print the traceback for debugging
+                return redirect('/')
+        return 1
 
     return app
 
--- flaskr/scraper.py
+++ flaskr/scraper.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from selenium import webdriver
 from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.keys import Keys
@@ -6,23 +6,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+import _sqlite3
 bp = Blueprint('scraper', __name__)
 
 @bp.route('/')
 def index():
     return render_template('index.html')
 
-# Create FirefoxOptions object
-firefox_options = Options()
-firefox_options.add_argument('--headless')  # Run Firefox in headless mode
-# Create a WebDriver instance using the Firefox driver and options
-driver = webdriver.Firefox(options=firefox_options)
-
 #driver = webdriver.Firefox()
 userFavoriteGenres = {'Fiction': 7, 'Feminism': 1, 'Contemporary': 5, 'Literary Fiction': 2, 'Asia': 1, 'Adult': 2, 'Audiobook': 6, 'Science Fiction': 3, 'Fantasy': 4, 'Dystopia': 3, 'Romance': 6, 'Young Adult': 6, 'Young Adult Contemporary': 2, 'Realistic Fiction': 2, 'Young Adult Romance': 3, 'Young Adult Fantasy': 1, 'Magic': 1, 'High Fantasy': 1}
 #userFavoriteGenres = {}
 bookScores = {}
+
 
 def findBookGenres(bookDriver, dict):
     genres = bookDriver.find_elements(By.CLASS_NAME, "BookPageMetadataSection__genreButton")
@@ -36,9 +31,17 @@ def findBookGenres(bookDriver, dict):
 
     return 1
 
+@bp.route('/process_user_list', methods=['POST'])
 def analyzeUserList():
+    firefox_options = Options()
+    firefox_options.add_argument('--headless')  # Run Firefox in headless mode
+    # Create a WebDriver instance using the Firefox driver and options
+    driver = webdriver.Firefox(options=firefox_options)
+
     # insert link to YOUR OWN list of books you want to find a similar one to
     driver.get('https://www.goodreads.com/review/list/99850740-hiba?ref=nav_mybooks&shelf=favorites')  # open GR site with the list
+    #inputLink = request.form['userList']
+    #driver.get(inputLink)
     print(driver.title)
 
     booksTable = driver.find_element(By.ID, "booksBody")
@@ -64,13 +67,20 @@ def analyzeUserList():
         listGenres = findBookGenres(driver2, userFavoriteGenres)
 
         driver2.close()
-
+    driver.close()
     print(userFavoriteGenres)
 
+@bp.route('/process_public_list', methods=['POST'])
 def analyzePublicList():
-    #driver.get('https://www.goodreads.com/list/show/63.Favorite_Books')
-    driver.get('https://www.goodreads.com/list/show/154685.TOP_YA_Dystopian_Books_of_ALL_TIME')
+    firefox_options = Options()
+    firefox_options.add_argument('--headless')  # Run Firefox in headless mode
+    # Create a WebDriver instance using the Firefox driver and options
+    driver = webdriver.Firefox(options=firefox_options)
 
+    #inputLink = request.form['publicList']
+    driver.get('https://www.goodreads.com/list/show/63.Favorite_Books')
+    #driver.get('https://www.goodreads.com/list/show/154685.TOP_YA_Dystopian_Books_of_ALL_TIME')
+    #driver.get(inputLink)
     booksTable = driver.find_element(By.ID, "all_votes")
 
     rows = booksTable.find_elements(By.TAG_NAME, "tr")  # get all of the rows in the table
@@ -86,7 +96,7 @@ def analyzePublicList():
 
         book_link = bookClass.get_attribute('href')
         # New browser to loop thru the books so that the for loop of the initial page is preserved
-        driver2 = webdriver.Firefox()
+        driver2 = webdriver.Firefox(options=firefox_options)
         # Navigate to a new URL in the newly opened window or tab
         print(book_link)
         driver2.get(book_link)
@@ -95,6 +105,7 @@ def analyzePublicList():
         genres = driver2.find_elements(By.CLASS_NAME, "BookPageMetadataSection__genreButton")
         for genre in genres:
             a = genre.find_element(By.TAG_NAME, "a")
+            # ADD TRY HERE -----------------
             nameOfGenre = a.find_element(By.TAG_NAME, "span").text
             if nameOfGenre in userFavoriteGenres:
                 score += 1
@@ -103,13 +114,15 @@ def analyzePublicList():
             bookScores[book_title] = score
 
         driver2.close()
+    driver.close()
+    sorted_scores = dict(sorted(bookScores.items(), key=lambda item: item[1], reverse=True))
 
     print("Based on your favorite genres, you would enjoy the following books, starting with the best recommendation: ")
-    for rec in bookScores:
-        print(rec, "with a score of", bookScores[rec])
-    print(bookScores)
+    for rec in sorted_scores:
+        print(rec, "with a score of", sorted_scores[rec])
+    #print(bookScores)
 
 
-#analyzeUserList()
+analyzeUserList()
 analyzePublicList()
-driver.close()  # close the browser
+
